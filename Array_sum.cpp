f878000ae51cc80151c43feaf4fe9ca2851f6a3f
goyalvansh4 <97226346+goyalvansh4@users.noreply.github.com>
2022-01-06 16:15:13 +0530

    Array_sum.cpp

---
+++ Array_sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int sum(int *a , int size) {
+  
+  int ans=0;
+  for(int i=0;i<n;i++)
+  {
+    ans += a[i];
+  }
+  return ans;
+}
+
+int main(){
+  
+  int arr[5] = {12,15,43,56,70};
+  
+  cout<<sum(a,5)<<endl;
+  
+  return 0;
+}
